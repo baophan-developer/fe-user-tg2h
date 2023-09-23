78ae45d8e52d53fbb6243a58dbf1456e91cff870
baophan-developer <baophan.developer@gmail.com>
2023-09-23 15:39:29 +0700

    feature: update image

---
+++ src/constants/image.ts
@@ -0,0 +1,8 @@
+const IMAGE_TYPE = {
+    JPEG: "image/jpeg",
+    PNG: "image/png",
+};
+
+const ACCEPT_IMAGE_SIZE = 1024 * 1024 * 2;
+
+export { IMAGE_TYPE, ACCEPT_IMAGE_SIZE };
--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import PubSub from "pubsub-js";
 import dayjs from "dayjs";
 import styled from "styled-components";
-import { Form, message } from "antd";
+import { Form, UploadFile, message, Upload, Image, Button } from "antd";
+import { RcFile, UploadChangeParam } from "antd/es/upload";
 import { useRecoilValue } from "recoil";
 import UserAtom from "@/stores/UserStore";
 import UserLayout from "@/layouts/UserLayout";
@@ -18,16 +19,98 @@ import {
 import request from "@/services/request";
 import { API_ENDPOINT } from "@/constants/apis";
 import PUBSUB_SUBSCRIBE_NAME from "@/constants/pubsub";
+import { UploadOutlined } from "@ant-design/icons";
+import { ACCEPT_IMAGE_SIZE, IMAGE_TYPE } from "@/constants/image";
 
 const ContentStyled = styled.div`
     width: 100%;
     display: flex;
 `;
 
+const BoxAvatarStyled = styled.div`
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px 0;
+`;
+
+const UploadButtonStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+`;
+
+const getBase64 = (img: RcFile, callback: (url: string) => void) => {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => callback(reader.result as string));
+    reader.readAsDataURL(img);
+};
+
 export default function Account() {
     const user = useRecoilValue(UserAtom);
     const [form] = Form.useForm();
     const [loading, setLoading] = useState<boolean>(false);
+    const [file, setFile] = useState<UploadFile | Blob>();
+    const [imgUrl, setImgUrl] = useState<string>();
+
+    const handleUploadAvatar = async () => {
+        setLoading(true);
+        try {
+            if (file) {
+                const form = new FormData();
+                form.append("avatar", file as Blob);
+                const res = await request<any>(
+                    "put",
+                    API_ENDPOINT.PROFILE.UPDATE_AVATAR,
+                    form
+                );
+                message.success(res.data.message, 1);
+                PubSub.publishSync(PUBSUB_SUBSCRIBE_NAME.GET_INFO);
+            } else {
+                message.info("Bạn chưa chọn ảnh đại diện mới.");
+            }
+        } catch (error: any) {
+            message.error(error.response.data.message, 1);
+        }
+        setLoading(false);
+    };
+
+    const handleOnChangeUpload = (info: UploadChangeParam<UploadFile>) => {
+        const file = info.fileList[0];
+        let checkSize = false;
+        let checkType = false;
+
+        if (file.size && file.type) {
+            checkSize = file.size < ACCEPT_IMAGE_SIZE;
+            checkType = [IMAGE_TYPE.JPEG, IMAGE_TYPE.PNG].includes(file.type);
+        }
+
+        if (!checkSize) {
+            message.error(
+                `Hình ảnh chỉ được phép dưới ${ACCEPT_IMAGE_SIZE / (1024 * 2)}MB`
+            );
+            info.fileList.pop();
+            return;
+        }
+
+        if (!checkType) {
+            message.error(
+                `Chỉ chấp nhận các tệp ảnh có định dạng là ${IMAGE_TYPE.JPEG} và ${IMAGE_TYPE.PNG}`
+            );
+            info.fileList.pop();
+            return;
+        }
+
+        if (file.originFileObj) {
+            getBase64(file.originFileObj as RcFile, (url) => {
+                setImgUrl(url);
+            });
+        }
+        setFile(file.originFileObj);
+        info.fileList.pop();
+    };
 
     const updateInfo = async (value: any) => {
         setLoading(true);
@@ -46,6 +129,7 @@ export default function Account() {
 
     useEffect(() => {
         form.setFieldsValue({ ...user, birthday: dayjs(user.birthday) });
+        setImgUrl(user.avatar);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [user]);
 
@@ -57,13 +141,13 @@ export default function Account() {
                     onFinish: updateInfo,
                     labelCol: { span: 5 },
                     wrapperCol: { span: 16 },
-                    style: { width: "70%" },
+                    style: { width: "100%" },
                 }}
                 fields={[
                     getInputEmail({ disabled: true }),
                     getInputName(),
                     getInputPhoneNumber(),
-                    getInputChooseDay({ format: "YYYY-MM-DD" }),
+                    getInputChooseDay(),
                     getInputGender(),
                 ]}
                 bottomForm={{
@@ -76,7 +160,29 @@ export default function Account() {
                     },
                 }}
             />
-            <div></div>
+            <BoxAvatarStyled>
+                <Upload
+                    showUploadList={false}
+                    beforeUpload={() => false}
+                    onChange={handleOnChangeUpload}
+                    accept="image/*"
+                >
+                    <UploadButtonStyled>
+                        <Image
+                            src={imgUrl}
+                            alt={user.name}
+                            width={200}
+                            height={200}
+                            preview={false}
+                            style={{ cursor: "pointer", objectFit: "contain" }}
+                        />
+                        <Button icon={<UploadOutlined />}>Chọn ảnh</Button>
+                    </UploadButtonStyled>
+                </Upload>
+                <Button loading={loading} type="primary" onClick={handleUploadAvatar}>
+                    Lưu
+                </Button>
+            </BoxAvatarStyled>
         </ContentStyled>
     );
 }
